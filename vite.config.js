40d15a6e6319df027af7736fb77b93a894b7aae0
Sathishkumar0404 <sathish@platformatory.com>
2024-11-07 11:14:46 +0530

    github page config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/karnataka-itihasa-academy",
     define: {
       'process.env':{}
     },
